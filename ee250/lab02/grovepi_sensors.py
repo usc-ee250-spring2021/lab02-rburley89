de20f00f36d26d08ecb1938e236371dba2558f18
Randi Burley <rburley@usc.edu>
2021-02-08 10:36:20 -0800

    reretry

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 
         rotary_value = grovepi.ultrasonicRead(potentiometer)
         ultra_value = grovepi.analogRead(potentiometer)
-        grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm\n" + str(rotary_value) + " cm)
+        grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm\n" + str(rotary_value) + " cm")
 
         if ultra_value <= rotary_value:
                 grove_rgb_lcd_setText_norefresh(str(ultra_value) + " cm OBJ PRES\n" + str(rotary_value) + " cm")
