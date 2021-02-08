2f3ccfd1e30e4ede3416edca5260f4eb3da08aa9
Randi Burley <rburley@usc.edu>
2021-02-08 11:59:29 -0800

    OH7

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                 if ultra_value <= rotary_value:
                         grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm OBJ PRES\n" + str(rotary_value) + " cm")
                 elif ultra_value >= rotary_value:
-                        grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm                \n" + str(rotary_value) + " cm")
+                        grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm          \n" + str(rotary_value) + " cm")
 
         except KeyboardInterrupt:
                 grovepi.analogWrite(led,0)
