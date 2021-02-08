9e9bd8a00c41b0f24e401dbfc93ccbb8681fff4f
Randi Burley <rburley@usc.edu>
2021-02-08 11:42:29 -0800

    OH5

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -56,12 +56,12 @@ if __name__ == '__main__':
 
                 rotary_value = grovepi.analogRead(potentiometer)
                 ultra_value = grovepi.ultrasonicRead(PORT)
-                grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm\n" + str(rotary_value) + " cm")
+                #grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm\n" + str(rotary_value) + " cm")
 
                 if ultra_value <= rotary_value:
                         grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm OBJ PRES\n" + str(rotary_value) + " cm                ")
                 elif ultra_value >= rotary_value:
-                        grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm\n" + str(rotary_value) + " cm                ")
+                        grove_rgb_lcd.setText_norefresh(str(ultra_value) + " cm                \n" + str(rotary_value) + " cm                ")
 
         except KeyboardInterrupt:
                 grovepi.analogWrite(led,0)
